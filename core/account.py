289748c9add250ea36afabc0e66fc52815db8b28
r virinchi <rvirinchi02@gmail.com>
2025-11-06 15:16:27 +0530

    added account class

--- core/account.py
+++ core/account.py
@@ -1,1 +1,32 @@
 # this is the account class
+
+import pandas as pd
+
+class account:
+    # instance variables
+    def __init__(self, username, account_number, balance = 0.00, acc_type = "SAVINGS"):
+        self.username = username
+        self.account_number = account_number
+        self.balance = balance
+        self.acc_type = acc_type
+
+    # instance methods
+    def deposit(self, amount):
+        if amount <= 0:
+            return False
+        self.balance += amount
+        return True
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return False
+        self.balance -= amount
+        return True
+    
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'account_number': self.account_number,
+            'balance': self.balance,
+            'acc_type': self.acc_type
+        }
