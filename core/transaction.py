6166e483767e34e3ed78729488971770fa86f354
r virinchi <rvirinchi02@gmail.com>
2025-11-06 15:26:19 +0530

    added transaction class

--- core/transaction.py
+++ core/transaction.py
@@ -1,1 +1,19 @@
 # this holds the transaction class
+
+import datetime as dt
+
+class transaction:
+    # instance variable
+    def __init__(self, from_username, to_username, amount):
+        self.from_username = from_username
+        self.to_username = to_username
+        self.amount = amount
+        self.datetime = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    def to_dict(self):
+        return {
+            'from_username': self.from_username,
+            'to_username': self.to_username,
+            'amount': self.amount,
+            'date_time': self.datetime
+        }
