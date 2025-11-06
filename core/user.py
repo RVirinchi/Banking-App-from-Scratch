dca8b8b9767253ec35dde9efee1893b939f7bc39
r virinchi <rvirinchi02@gmail.com>
2025-11-06 15:08:05 +0530

    added user class

--- core/user.py
+++ core/user.py
@@ -1,1 +1,19 @@
 # this is the user class
+
+import hashlib as hl
+
+class user:
+    # instance variables
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password_hash = self._hash_password(password)
+        self.name = name
+        self.email = email
+
+    def _hash_password(self, password):
+        return hl.sha256(password.encode('utf-8')).hexdigest()
+    
+    def verify_password(self, password):
+        return hl.sha256(password.encode('utf-8')).hexdigest() == self.password_hash
+
+
