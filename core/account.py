1a89ab3d6d0090426d42aaba5e974843f27215c6
r virinchi <rvirinchi02@gmail.com>
2025-11-07 13:42:49 +0530

    added transfer feature to account

--- core/account.py
+++ core/account.py
@@ -21,6 +21,7 @@ class account:
         self.balance += amount
         trnsxn_obj = transaction(self.username, self.username, amount)
         trnsxn_obj.record_transaction()
+        self.save_account()
         return True
     
     def withdraw(self, amount):
@@ -29,10 +30,21 @@ class account:
         self.balance -= amount
         trnsxn_obj = transaction(self.username, self.username, amount * -1) # withdraw is negetive
         trnsxn_obj.record_transaction()
+        self.save_account()
+        return True
+    
+    def transfer(self, to_username, amount) -> bool:
+        if amount < 0 or amount < self.balance:
+            return False
+        self.balance -= amount
+        trnsxn_obj = transaction(self.username, to_username, amount)
+        trnsxn_obj.record_transaction()
+        self.save_account()
         return True
     
     def save_account(self):
         df = pd.read_csv(self.ACC_FILE)
+        df = df[df['username'] != self.username]
         df.loc[len(df)] = self.to_dict()
         df.to_csv(self.ACC_FILE, index=False)
 
@@ -40,12 +52,7 @@ class account:
     @classmethod
     def load_account(cls, username):
         df = pd.read_csv(account.ACC_FILE)
-        acc_data = df[df['username'] == username]
-        
-        # if no account exists with that username then return none
-        if acc_data.empty:
-            return None
-        
+        acc_data = df[df['username'] == username]   
         return account(acc_data['username'], acc_data['acc_number'], acc_data['balance'], acc_data['acc_type'])
     
     def to_dict(self) -> dict:
